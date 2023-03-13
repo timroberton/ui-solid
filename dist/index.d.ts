5e5b41fc046ddafd45090ab1b7e87636587326bd
timroberton <timroberton@gmail.com>
2023-03-13 09:20:12 +0800

    Deploy at 2023_03_13_09_20_12

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
+export { default as AlertProvider, useAlert } from "./alert";
+export type { AlertComponentProps, AlertContext } from "./alert";
 export * from "./button";
 export * from "./input";
 export * from "./select";
-export { default as AlertProvider, useAlert } from "./alert";
-export type { AlertContext, AlertComponentProps } from "./alert";
 //# sourceMappingURL=index.d.ts.map
